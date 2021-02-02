c87e7615c147b8bb471ea7b409f76c6045f7cb40
Stuart Bowes <stuartbowes2000@gmail.com>
2021-02-02 16:59:09 +0000

    Add Create and Edit Customer pages

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,5 +1,7 @@
 using System.Linq;
+using System.Threading.Tasks;
 using CustomerDetails.DomainLayer;
+using CustomerDetails.Models;
 using CustomerDetails.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -46,6 +48,77 @@ namespace CustomerDetails.Controllers
                 PostCode = customer.PostCode
             };
             return View(viewModel);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateCustomerViewModel model)
+        {
+            var customer = new Customer
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                BusinessName = model.BusinessName,
+                BuildingName = model.BuildingName,
+                NumberAndStreet = model.NumberAndStreet,
+                LocalityName = model.LocalityName,
+                TownOrCity = model.TownOrCity,
+                County = model.County,
+                PostCode = model.PostCode
+            };
+
+            _customerService.Add(customer);
+
+            // Redirect back to the list
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var customer = _customerService.Find(id);
+            var viewModel = new EditCustomerViewModel
+            {
+                Id = customer.Id,
+                Form = new CustomerFormViewModel
+                {
+                    FirstName = customer.FirstName,
+                    LastName = customer.LastName,
+                    BusinessName = customer.BusinessName,
+                    BuildingName = customer.BuildingName,
+                    NumberAndStreet = customer.NumberAndStreet,
+                    LocalityName = customer.LocalityName,
+                    TownOrCity = customer.TownOrCity,
+                    County = customer.County,
+                    PostCode = customer.PostCode
+                }
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditCustomerViewModel model)
+        {
+            var customer = new Customer
+            {
+                FirstName = model.Form.FirstName,
+                LastName = model.Form.LastName,
+                BusinessName = model.Form.BusinessName,
+                BuildingName = model.Form.BuildingName,
+                NumberAndStreet = model.Form.NumberAndStreet,
+                LocalityName = model.Form.LocalityName,
+                TownOrCity = model.Form.TownOrCity,
+                County = model.Form.County,
+                PostCode = model.Form.PostCode
+            };
+
+            _customerService.Update(customer);
+
+            // Redirect back to the list
+            return RedirectToAction(nameof(Index));
         }
     }
 }
